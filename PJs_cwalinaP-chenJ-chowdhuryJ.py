791ebe5fd5f79baaa32c6e7c4b1751e09ca64844
jchen59 <jchen59@stuy.edu>
2018-09-14 08:40:55 -0400

    added occupation for got to push :(

---
+++ PJs_cwalinaP-chenJ-chowdhuryJ.py
@@ -0,0 +1,36 @@
+#PJs -- Peter Cwalina $ Jiayang Chen & Jabir Chowdhury
+#SoftDev1 pd7
+#K06 -- StI/O: Divine your Destiny!
+#2018-09-13
+
+from random import random
+
+def make_dictionary(fname):
+    f = open(fname) # Opens the file
+    lines = f.read().strip('\n').split('\n') # Makes a list of all the lines
+    dict = {}
+    for name in lines[1:-1]:
+        if name[0] == '"': # If there are quotations
+            words = name.strip('"').split('"')
+            words[1] = words[1].strip(',') # Gets rip of comma
+        else: # If there are no quotations
+            words = name.split(",")
+
+        dict[words[0]] = words[1] # Matches job titles with percentage
+
+    return dict
+
+#Picks a random number out of 100
+#Loops until total is above the random number and returns the values
+def random_job(dict):
+    q = random() * 100 # Random value 0 to 1 incl -> 0 to 100 incl
+    current = 0
+    for key in dict:
+        current += float(dict[key])
+        if current > q:
+            return key + " with a " + dict[key] + "% chance"
+
+
+d = make_dictionary("occupations.csv")
+
+print(random_job(d))
