786c97d1b9735d4a5139478cc62b960513a7651d
jiayang <jchen59@stuy.edu>
2018-10-04 21:47:05 -0400

    renmoved quotes

--- 16_csv2db/app.py
+++ 16_csv2db/app.py
@@ -3,12 +3,8 @@
 #K16 -- No Trouble
 #2018-10-04
 
-from flask import Flask, render_template, request
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
-
-app = Flask(__name__) # instantiates an instance of Flask
-
 
 DB_FILE="foo.db"
 
@@ -18,31 +14,24 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
+# Create and populate the courses table
 with open('./data/courses.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     command = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)"
-    c.execute(command)    
+    c.execute(command)
     for row in reader:
-        command = "INSERT INTO courses VALUES(\" " + row['code']+"\"," +  row['mark'] + "," + row['id'] + ")"
+        command = 'INSERT INTO courses VALUES("' + row['code']+'",' +  row['mark'] + "," + row['id'] + ")"
         c.execute(command)
 
+# Create and populate the courses table
 with open('./data/peeps.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     command = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)"
-    c.execute(command)   
+    c.execute(command)
     for row in reader:
-        command = "INSERT INTO peeps VALUES(\" " + row['name']+"\"," +  row['age'] + "," + row['id'] + ")"
+        command = 'INSERT INTO peeps VALUES("' + row['name']+'",' +  row['age'] + "," + row['id'] + ")"
         c.execute(command)
 #==========================================================
 
 db.commit() #save changes
 db.close() #close database
-
-
-@app.route("/") #Linking a function to a route
-def home():
-    return "Hello, World!"
-
-if __name__ != "__main__":
-    app.debug = True
-    app.run()
